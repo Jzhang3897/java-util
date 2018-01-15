72f484a8b2626e294de020ce2e84088a0fedcc8e
zhangjun16 <zhangjun16@jd.com>
2018-01-15 15:47:43 +0800

    sql entity建立

--- src/main/java/com/zj/util/orm/GenerateEntity.java
+++ src/main/java/com/zj/util/orm/GenerateEntity.java
@@ -1,25 +1,61 @@
 package com.zj.util.orm;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by zhangjun16 on 2018/1/15.
  */
 public class GenerateEntity {
-    public static void main(String[] args) {
-        String column = "id\n" +
-                "active_id\n" +
-                "sku\n" +
-                "comment_id\n" +
-                "guid\n" +
-                "pin\n" +
-                "word_count\n" +
-                "show_count\n" +
-                "video_count\n" +
-                "status\n" +
-                "create_time\n" +
-                "modify_time\n";
-        String[] columns = column.split("\n");
-        for (int i = 0; i < columns.length; i++) {
-            System.out.printf("private ");
+    private static String SPLIT_CHAR = "\t";
+
+    public static void main(String[] args) throws Exception {
+        generateEntity("D:\\data\\reward1.txt");
+        generateEntity("www");
+    }
+    private static void generateEntity(String fileName) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        String line;
+        while ((line = bf.readLine()) != null) {
+            String[] columns = line.split(SPLIT_CHAR);
+            if ("id".equals(columns[0])) {
+                System.out.println("@Id");
+                System.out.println("@GeneratedValue(strategy = GenerationType.IDENTITY)");
+            }
+            System.out.println("@Column(name = \"" + columns[0] + "\")");
+            System.out.println("private " + typeOf(columns[1]) + " " + nameOf(columns[0]) + ";");
         }
+        bf.close();
+    }
+    private static String typeOf(String sqlType){
+        String type;
+        switch (sqlType){
+            case "bigint": type ="Long";break;
+            case "varchar": type ="String";break;
+            case "int": type ="Integer";break;
+            case "tinyint": type ="int";break;
+            case "datetime": type ="Date";break;
+            case "text": type ="String";break;
+            case "double": type ="double";break;
+            case "float": type ="float";break;
+            case "smallint": type ="int";break;
+            case "integer": type ="int";break;
+            case "decimal": type ="BigDecimal";break;
+            case "char": type = "char";break;
+            default: type = sqlType;
+    }
+        return type;
+    }
+
+    private static String nameOf(String name) {
+        String[] words = name.split("_");
+        StringBuilder sb = new StringBuilder();
+        sb.append(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            sb.append(Character.toUpperCase(words[i].charAt(0)));
+            sb.append(words[i].substring(1, words[i].length()));
+        }
+        return sb.toString();
     }
 }
